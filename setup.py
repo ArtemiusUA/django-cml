6b43a43b87fd7a46310f2e30c832b6b68d56ea9f
mikamiel <mikamiel.k@gmail.com>
2019-10-12 23:01:48 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-cml',
+    python_requires='>3.3.0'
     version='0.3.5',
     packages=['cml'],
     install_requires=['Django>=2.0', 'django-appconf>=1.0.1', 'six>=1.12.0'],
@@ -26,7 +27,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
